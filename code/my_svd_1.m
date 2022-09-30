35ac063ad18ab807e6152f1c530bd69ad07cd713
gilsaia <794433219@qq.com>
2022-09-30 23:01:45 +0800

    feat: change svd1

--- code/my_svd_1.m
+++ code/my_svd_1.m
@@ -1,6 +1,15 @@
 function [u,s,v]=my_svd_1(A,r)
     
-    [u,s,v]=jacobi_svd(A);
-    % [u,s,v]=svd(A);
+    tic;
+    [U,B,V]=bidiagnoal_r(A,r);
+    bitime=toc;
+    [m,n]=size(B);
+    Bo=B(1:n,:);
+    disp(sprintf('Bidiagnoal time %f',bitime));
+    tic;
+    [Ux,Bt,Vx]=bidiagnoal_svd(Bo);
+    bisvdtime=toc;
+    disp(sprintf('Bidiagnoal time %f,SVD time %f', bitime,bisvdtime));
+    [u,s,v]=svd(A);
 
 end
