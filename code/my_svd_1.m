6fcb70109d7dff8f9f5b4f8a398837c02b7276c1
gilsaia <794433219@qq.com>
2022-10-09 02:20:20 +0800

    feat:add withoutu

--- code/my_svd_1.m
+++ code/my_svd_1.m
@@ -1,7 +1,17 @@
 function [u,s,v]=my_svd_1(A,r)
     [m,n]=size(A);
     [u,b,v]=bidiagonal_r(A,r+1,m,n);
-    [u(:,1:n),s,v]=dk_svd(b,u(:,1:n),v,n);
+    if r>256
+        % prepare b
+        bt=[b';zeros(1,n)];
+        [ut,s,vt]=dc_svd(b,n,r);
+        % dc output ut is v,vt is u
+        ut=ut(1:n,1:n);
+        v=matmulf(v,ut);
+        u=matmulf(u,[vt zeros(n,m-n);zeros(m-n,n) eye(m-n,m-n)]);
+    else
+        [u(:,1:n),s,v]=dk_svd(b,u(:,1:n),v,n);
+    end
     [s,v]=change_signval(s,v,r);
     s=[s;zeros(m-n)];
 end
